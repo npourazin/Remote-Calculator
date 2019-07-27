e0fcf506f0806b3b442ad12ef68d1a103a16d37e
Niki Pourazin <nikipourzin@gmail.com>
2019-07-27 17:13:33 +0430

    bug fixed! needed break statements added

--- Client/Client.go
+++ Client/Client.go
@@ -7,17 +7,18 @@ import (
 	"os"
 )
 
-func main()  {
+func main() {
 	connection, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Println("Enter:")
 		nums, _ := reader.ReadString('\n')
 		//fmt.Println(nums)
-		if nums=="quit\n"{
+		if nums == "quit\n" {
 			fmt.Println("suicide")
 			break
 		}
@@ -33,10 +34,10 @@ func main()  {
 
 		res, _ := bufio.NewReader(connection).ReadString('\n')
 
-
 		fmt.Println("Result:", res)
 	}
 	_, err = connection.Write([]byte("quit"))
+	//unhandled error
 	connection.Close()
 	return
 
--- Server/Server.go
+++ Server/Server.go
@@ -8,11 +8,12 @@ import (
 	"strconv"
 )
 
-func main()  {
+func main() {
 	client, err := net.Listen("tcp", ":8080")
 
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	for {
@@ -20,102 +21,105 @@ func main()  {
 		connection, err := client.Accept()
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
 		for {
 			receive, err := bufio.NewReader(connection).ReadString('.')
 			if err != nil {
 				fmt.Println(err)
+				break
 			}
 
 			ans := calculate(receive)
-			fmt.Println(receive+" = "+strconv.Itoa(ans))
+			fmt.Println(receive + " = " + strconv.Itoa(ans))
 
-			if receive=="quit"{
+			if receive == "quit" {
 				break
 			}
 
 			//errors are to be handled below
-			if 1 == 0 {
-				_, err = connection.Write([]byte("Invalid Input!"))
-				if err != nil {
-					fmt.Println(err)
-				}
-				connection.Close()
-				return
-			}
+			//if 1 == 0 {
+			//	_, err = connection.Write([]byte("Invalid Input!"))
+			//	if err != nil {
+			//		fmt.Println(err)
+			//	}
+			//	connection.Close()
+			//	return
+			//}
 
-			_, err = connection.Write([]byte(strconv.Itoa(ans)+"\n"))
+			_, err = connection.Write([]byte(strconv.Itoa(ans) + "\n"))
 			if err != nil {
 				fmt.Println(err)
+				break
 			}
 		}
 		connection.Close()
+		//unhandled error
+
 	}
 
 }
 
-
-
-func calculate(n string) int{
-	text := n[0:len(n)]
-	if len(n)>0 && n[len(n)-1]=='\n'{
-		text = n[0:len(n)-1]
+func calculate(n string) int {
+	text := n
+	if len(n) > 0 && n[len(n)-1] == '\n' {
+		text = n[0 : len(n)-1]
 	}
 	//or -3
 	argis := []int{}
-	j :=0
-	for i:=0;i< len(text);i++  {
-		if text[i]=='+' {
+	j := 0
+	for i := 0; i < len(text); i++ {
+		if text[i] == '+' {
 			s := text[j:i]
-			if j!=0{
-				d := text[j-1:j]
+			if j != 0 {
+				d := text[j-1 : j]
 				//fmt.Println(text[j-1:j])
-				s = d+s
+				s = d + s
 			}
 
-			j=i+1
-			if j>len(text) {
-				break;
+			j = i + 1
+			if j > len(text) {
+				break
 			}
-			p,_ :=strconv.Atoi(s)
+			p, _ := strconv.Atoi(s)
 			argis = append(argis, p)
 			//fmt.Println("--",p)
 		}
-		if text[i]=='-' {
+		if text[i] == '-' {
 			s := text[j:i]
-			if j!=0{
-				d := text[j-1:j]
+			if j != 0 {
+				d := text[j-1 : j]
 				//fmt.Println(text[j-1:j])
-				s = d+s
+				s = d + s
 			}
 			d := text[j:j]
-			s = d+s
-			j=i+1
-			if j>len(text) {
-				break;
+			s = d + s
+			j = i + 1
+			if j > len(text) {
+				break
 			}
-			p,_ :=strconv.Atoi(s)
+			p, _ := strconv.Atoi(s)
 			argis = append(argis, p)
 			//fmt.Println("--",p)
 		}
 	}
-	if j<len(text){
-		s := text[j:len(text)-1]
-		if j!=0{
-			d := text[j-1:j]
+	if j < len(text) {
+		s := text[j : len(text)-1]
+		if j != 0 {
+			d := text[j-1 : j]
 			//fmt.Println(text[j-1:j])
-			s = d+s
+			s = d + s
 		}
-		p,_ :=strconv.Atoi(s)
+		p, _ := strconv.Atoi(s)
 		argis = append(argis, p)
 		//fmt.Println("--",p)
 	}
 
 	//fmt.Println(len(text))
 	//fmt.Println("len ",len(argis), "\n", argis)
-	sum:=0
-	for i:=0;i< len(argis);i++  {
-		sum+=argis[i]
+	sum := 0
+	for i := 0; i < len(argis); i++ {
+		sum += argis[i]
 	}
 	//fmt.Println(sum)
 	return sum
