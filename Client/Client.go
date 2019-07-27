4102f8a2393d2b42b14e1336003294b67a34632d
Niki Pourazin <nikipourzin@gmail.com>
2019-07-27 17:02:14 +0430

    initial

---
+++ Client/Client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main()  {
+	connection, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println("Enter:")
+		nums, _ := reader.ReadString('\n')
+		//fmt.Println(nums)
+		if nums=="quit\n"{
+			fmt.Println("suicide")
+			break
+		}
+
+		//prepare the string to be sent
+		write := nums[0:len(nums)-1] + "."
+
+		_, err = connection.Write([]byte(write))
+		if err != nil {
+			fmt.Println(err)
+		}
+		//fmt.Println("request sent")
+
+		res, _ := bufio.NewReader(connection).ReadString('\n')
+
+
+		fmt.Println("Result:", res)
+	}
+	_, err = connection.Write([]byte("quit"))
+	connection.Close()
+	return
+
+}
+
+/**
+Issues:
+1- if file is closed server is dooooomed
+2- wrong input not handled
+*/
---
+++ Server/Server.go
@@ -0,0 +1,131 @@
+//package Server
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strconv"
+)
+
+func main()  {
+	client, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for {
+
+		connection, err := client.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+		for {
+			receive, err := bufio.NewReader(connection).ReadString('.')
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			ans := calculate(receive)
+			fmt.Println(receive+" = "+strconv.Itoa(ans))
+
+			if receive=="quit"{
+				break
+			}
+
+			//errors are to be handled below
+			if 1 == 0 {
+				_, err = connection.Write([]byte("Invalid Input!"))
+				if err != nil {
+					fmt.Println(err)
+				}
+				connection.Close()
+				return
+			}
+
+			_, err = connection.Write([]byte(strconv.Itoa(ans)+"\n"))
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		connection.Close()
+	}
+
+}
+
+
+
+func calculate(n string) int{
+	text := n[0:len(n)]
+	if len(n)>0 && n[len(n)-1]=='\n'{
+		text = n[0:len(n)-1]
+	}
+	//or -3
+	argis := []int{}
+	j :=0
+	for i:=0;i< len(text);i++  {
+		if text[i]=='+' {
+			s := text[j:i]
+			if j!=0{
+				d := text[j-1:j]
+				//fmt.Println(text[j-1:j])
+				s = d+s
+			}
+
+			j=i+1
+			if j>len(text) {
+				break;
+			}
+			p,_ :=strconv.Atoi(s)
+			argis = append(argis, p)
+			//fmt.Println("--",p)
+		}
+		if text[i]=='-' {
+			s := text[j:i]
+			if j!=0{
+				d := text[j-1:j]
+				//fmt.Println(text[j-1:j])
+				s = d+s
+			}
+			d := text[j:j]
+			s = d+s
+			j=i+1
+			if j>len(text) {
+				break;
+			}
+			p,_ :=strconv.Atoi(s)
+			argis = append(argis, p)
+			//fmt.Println("--",p)
+		}
+	}
+	if j<len(text){
+		s := text[j:len(text)-1]
+		if j!=0{
+			d := text[j-1:j]
+			//fmt.Println(text[j-1:j])
+			s = d+s
+		}
+		p,_ :=strconv.Atoi(s)
+		argis = append(argis, p)
+		//fmt.Println("--",p)
+	}
+
+	//fmt.Println(len(text))
+	//fmt.Println("len ",len(argis), "\n", argis)
+	sum:=0
+	for i:=0;i< len(argis);i++  {
+		sum+=argis[i]
+	}
+	//fmt.Println(sum)
+	return sum
+}
+
+/**
+Issues:
+1- if file is closed server is dooooomed
+2- wrong input not handled
+3- explain input way to the idiot client
+4- handle multiple clients?
+
+*/
